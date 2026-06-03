f2161e1adf3e6a8a99b0e0570ec2a28ecba7c1da
Ricardo Campos <ricardompcampos@gmail.com>
2026-06-03 15:27:06 +0200

    chore: update containers names and flows for better naming

--- nginx/start-proxy.sh
+++ nginx/start-proxy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Stop and remove existing proxy if it exists
-docker stop ngrok-proxy 2>/dev/null
-docker rm ngrok-proxy 2>/dev/null
+#docker stop nginx-proxy 2>/dev/null
+#docker rm nginx-proxy 2>/dev/null
 
-docker run -d \
-  --name ngrok-proxy \
+docker run -d --rm \
+  --name nginx-proxy \
   -p 127.0.0.1:8080:8080 \
   -v "$(pwd)/nginx/nginx.conf:/etc/nginx/nginx.conf:ro" \
-  --restart unless-stopped \
   --network polpa-network \
   nginx:stable
+
